58be4b22aecbaf0f0fe08182e15548c6725d8576
Joyson <cj4dev@gmail.com>
2025-01-17 07:50:37 +0530

    cj

--- frontend/AuctionApp/src/App.jsx
+++ frontend/AuctionApp/src/App.jsx
@@ -1,45 +1,29 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Auction from './pages/Auction';
 import About from './pages/About';
 import Product from './pages/Product';
-import ProductManager from './ItsMeDaddy/ProductManager.jsx';
-import Register from './ItsMeDaddy/Register.jsx';
+// import ProductManager from './ItsMeDaddy/ProductManager.jsx';
+// import Register from './ItsMeDaddy/Register.jsx';
 
 function App() {
   return (
-<<<<<<< HEAD
     <div>
-    <Navbar />
-    <div className='mx-4 sm:mx-[5%]'>
-      
+      <Navbar />
+      <div className='mx-4 sm:mx-[5%]'>
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path='/' element={<Home />} />
           <Route path='/auctions' element={<Auction />} />
           <Route path='/about' element={<About />} />
           <Route path='/product/:id' element={<Product />} />
         </Routes>
-      <Footer />
+        <Footer />
+      </div>
+      {/* <ProductManager /> */}
     </div>
-    </div>
-=======
-    // <div className='mx-4 sm:mx-[5%]'>
-    //   <Navbar />
-    //     <Routes>
-    //       <Route path='/' element={<Home />}/>
-    //       <Route path='/auctions' element={<Auction />} />
-    //       <Route path='/about' element={<About />} />
-    //       <Route path='/product/:id' element={<Product />} />
-    //     </Routes>
-    //   <Footer />
-    // </div>
-
-    // <ProductManager/>
-    <Register/>
->>>>>>> 826ec3fd24bdbb3d18c12e1a02bc6c9c5e279af8
   );
 }
 
