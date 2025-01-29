9ca9287a6a7c2bfe53772eec3990a3cd7e3f778a
Rajakoone Christan <rajakoonechristan@gmail.com>
2025-01-30 05:16:43 +0530

    merging joyss

--- frontend/AuctionApp/src/App.jsx
+++ frontend/AuctionApp/src/App.jsx
@@ -1,27 +1,16 @@
 import React, { useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home';
 import Auction from './pages/Auction';
 import About from './pages/About';
 import Product from './pages/Product';
-import Login from './ItsMeDaddy/Login';
+
 
 function App() {
-  const [userRole, setUserRole] = useState(localStorage.getItem("userRole") || null);
-  const navigate = useNavigate();
-
-  const handleLogin = (role) => {
-    setUserRole(role);
-    if (role === "Admin") {
-      navigate("/admin-dashboard"); // Redirect Admin to dashboard
-    } else {
-      navigate("/"); // Redirect User to Home
-    }
-  };
-
   return (
     <div>
       <Navbar />
@@ -31,11 +20,12 @@ function App() {
           <Route path='/auctions' element={<Auction />} />
           <Route path='/about' element={<About />} />
           <Route path='/product/:id' element={<Product />} />
-          <Route path='/login' element={<Login onLogin={handleLogin} />} />
         </Routes>
         <Footer />
       </div>
+     
     </div>
+
   );
 }
 
